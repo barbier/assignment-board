f70197b4e448360e53a7092e6bbec32cf70afb2d
Gabriel 'Barbier <gabrielnasb@gmail.com>
2017-05-10 22:22:11 -0300

    The card now is created via javascript, no more big strings. More use of ES6

--- src/js/app.js
+++ src/js/app.js
@@ -1,22 +1,46 @@
+/**
+ * Returns a value from a input
+ * @param Query to get a input tag
+ * @return String
+ */
 function getValue(query) {
   let element = document.querySelector(query);
   
   return element.value;
 }
 
-function createAssignment(title, description) {
-  let template = `<div class='column'>
-  <h2>${title}</h2><p>${description}</p></div>`;
-  let node = document.getElementById("icebox");
-
-  node.innerHTML += template;
+/**
+ * Class to alocate both values of the form
+ */
+class Assignment {
+  constructor(title, description) {
+    this.title = title;
+    this.description = description;
+  }
 }
 
-function assignmentClass() {
-  this.title = getValue("#assignment");
-  this.description = getValue("#description");
+/**
+ * Creates the card element
+ */
+function createCard() {
+  let icebox = document.getElementById('icebox');
+  let cardContent = new Assignment(getValue('#assignment'), getValue('#description'));
+  let card = document.createElement('div');
+  let cardTitle = document.createElement('h2');
+  let cardDescription = document.createElement('p');
+
+  icebox.appendChild(card);
+
+  card.appendChild(cardTitle);
+  card.appendChild(cardDescription);
+
+  cardTitle.innerHTML = cardContent.title;
+  cardDescription.innerHTML = cardContent.description;
 }
 
+/**
+ * Clear the form after create the card
+ */
 function clearForm() {
   let assignment = document.getElementById('assignment');
   let description = document.getElementById('description');
@@ -30,9 +54,7 @@ function clearForm() {
   let button = document.getElementById("createButton");
 
   button.addEventListener('click', function() {
-    let assignment = new assignmentClass();
-
-    createAssignment(assignment.title, assignment.description);
+    createCard();
     clearForm();
   }, false);
 }());
